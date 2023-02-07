7c464955fa4918df803ea399245101bf04edcf8e
JaerongA <jaerong.ahn@datajoint.com>
2023-02-07 15:41:05 -0600

    add SkullReference for ephys schema

--- workflow/pipeline/reference.py
+++ workflow/pipeline/reference.py
@@ -1,5 +1,14 @@
 import datajoint as dj
-
 from workflow import db_prefix
 
 schema = dj.schema(db_prefix + "reference")
+
+
+@schema
+class SkullReference(dj.Lookup):
+    definition = """
+    reference   : varchar(60)
+    """
+    contents = zip(
+        ["bregma", "lambda", "dura", "skull_surface", "sagittal_suture", "sinus"]
+    )
