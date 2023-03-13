b4ee65ee18b20aa3f8bb578408e4285669f2fb8f
Tolga Dincer <tolgadincer@gmail.com>
2023-03-13 15:09:09 -0500

    Add upload script

--- scripts/initiate_session.py
+++ scripts/initiate_session.py
@@ -4,11 +4,8 @@ import djsciops.axon as dj_axon
 import djsciops.settings as dj_settings
 import djsciops.authentication as dj_auth
 
-
-LOCAL_OUTBOX = pathlib.Path(R"E:/uploads")
-LOCAL_INBOX = pathlib.Path(R"E:/kilosort/axon")
-PROJECT_NAME = "utah-organoids"
-
+LOCAL_OUTBOX = pathlib.Path(R"/Users/tolgadincer/DJRepos/utah_organoids/inbox")
+PROJECT_NAME = "utah_organoids"
 
 config = dj_settings.get_config()
 s3_session = dj_auth.Session(
@@ -20,6 +17,15 @@ s3_bucket = config["s3"]["bucket"]
 
 
 def upload_session_data(session_dir_relpath):
+    """Upload a session's data.
+
+    Args:
+        session_dir_relpath (str): Relative session path
+
+    Example:
+        If the data is located at "/Users/tolgadincer/DJRepos/utah_organoids/inbox/O6/.."
+        >>> upload_session_data(O6)
+    """
     session_dir_relpath = pathlib.Path(session_dir_relpath).as_posix()
 
     local_session_dir = LOCAL_OUTBOX / session_dir_relpath
