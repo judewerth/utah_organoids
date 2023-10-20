62b225ae9256f0f15a3c7c7db31d16a9c5ca84cc
JaerongA <jaerong.ahn@datajoint.com>
2023-10-20 11:39:25 -0500

    feat: :sparkles: add get_repo_dir

--- src/workflow/utils/paths.py
+++ src/workflow/utils/paths.py
@@ -3,6 +3,7 @@ import datajoint as dj
 
 from element_interface.utils import find_full_path
 from workflow.pipeline import culture
+import subprocess
 
 
 def get_raw_root_data_dir() -> Path:
@@ -24,3 +25,11 @@ def get_subject_directory(experiment_key: dict) -> Path:
         get_ephys_root_data_dir(),
         (culture.Experiment & experiment_key).fetch1("experiment_directory"),
     )
+
+
+def get_repo_dir() -> Path:
+    """Get get root directory path"""
+    result = subprocess.run(
+        ["git", "rev-parse", "--show-toplevel"], stdout=subprocess.PIPE
+    )
+    return Path(result.stdout.decode().strip())
