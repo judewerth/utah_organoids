512e6ada1e0582b1894f78cc6199266df52c2cba
MilagrosMarin <milmarina02@gmail.com>
2025-06-30 14:23:28 +0100

    update worker

--- src/workflow/populate/worker.py
+++ src/workflow/populate/worker.py
@@ -44,8 +44,8 @@ spike_sorting_worker = DataJointWorker(
 
 # mua
 # standard_worker(mua.MUAEphysSession, max_calls=20)
-# standard_worker(mua.MUASpikes, max_calls=20)
-# standard_worker(mua.MUATracePlot, max_calls=20)
+standard_worker(mua.MUASpikes, max_calls=20)
+standard_worker(mua.MUATracePlot, max_calls=20)
 
 # ephys LFP
 standard_worker(ingestion_support.FileProcessing)
